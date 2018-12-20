ab24f48fe1bc48c348a95c7a09997707db74e30a
Daniel Honorio <daniel.holiveira@gmail.com>
2018-12-20 09:53:30 -0200

    exemplos INSERT DAO

--- index.php
+++ index.php
@@ -7,8 +7,16 @@
 
     echo json_encode($usuarios);*/
 
-    $user = new Usuario();
-    $user->loadById(6);
+//    $user = new Usuario();
+    //$user->loadById(6);
+//    echo $user;
+    //$lista = Usuario::getList();
+    
+/*    $search = Usuario::search("jose");
+    echo json_encode($search);*/
 
-    echo $user;
+    $usuario = new Usuario();
+    $usuario->login("user3","12345");
+
+    echo $usuario;
 ?>
